8230cdc44fa27bfa5110f001bb4a98993d96d05f
Kureev Alexey <kureev-mail@ya.ru>
2015-01-04 14:40:39 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp = require('gulp');
+var gReact = require('gulp-react');
+var gReplace = require('gulp-replace');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var del = require('del');
+
+var browserifyConfig = {
+  entries: ['./index.js'],
+  standalone: 'Fluxy'
+};
+
+gulp.task('clean', function(cb) {
+  del(['lib/', 'Fluxy.js'], cb);
+});
+
+gulp.task('lib', function() {
+  return gulp.src('src/*.js')
+             .pipe(gReact({harmony: true}))
+             .pipe(gReplace(/__DEV__/g, 'false'))
+             .pipe(gulp.dest('lib'));
+
+});
+
+gulp.task('browserify', function() {
+  return browserify(browserifyConfig)
+          .bundle()
+          .pipe(source('Fluxy.js'))
+          .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('publish', ['clean', 'default']);
+gulp.task('default', ['lib', 'browserify']);
