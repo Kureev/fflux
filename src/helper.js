c76d5882f1e78a4df1727b96cdf430387d631a2f
Kureev Alexey <kureev-mail@ya.ru>
2015-01-17 22:00:42 +0100

    Add comments to the helper functions

--- src/helper.js
+++ src/helper.js
@@ -1,17 +1,37 @@
 'use strict';
 
+/**
+ * Check if param is object
+ * @param  {any}  param Parameter to check
+ * @return {boolean}
+ */
 function isObject(param) {
     return Object.prototype.toString.call(param) === '[object Object]';  
 }
 
+/**
+ * Check if param is array
+ * @param  {any}  param Parameter to check
+ * @return {boolean}
+ */
 function isArray(param) {
     return Object.prototype.toString.call(param) === '[object Array]';
 }
 
+/**
+ * Get object's keys
+ * @param  {object} obj
+ * @return {array}  Array of the keys
+ */
 function keys(obj) {
     return Object.keys(obj);
 }
 
+/**
+ * Extend object by other obect(s)
+ * @param  {object} obj Object to extend
+ * @return {object}     Extended object
+ */
 function extend(obj) {
     if (!isObject(obj)) {
         return obj;
