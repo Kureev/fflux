e8b1c96bb4139341f62b0e4d7543918436b00388
Alexey Kureev <a.kureev@xs2theworld.com>
2015-01-23 13:46:06 +0100

    New dist files

--- dist/fflux.js
+++ dist/fflux.js
@@ -50,6 +50,14 @@ FFlux.mixins.bind = function(store) {
          */
         componentWillUnmount: function () {
             store.removeListener('change', this.storeDidUpdate);
+        },
+
+        /**
+         * Standart behaviour for `store` update
+         * @return {void}
+         */
+        storeDidUpdate: function() {
+            this.forceUpdate();
         }
     };
 };
