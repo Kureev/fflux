270da2a7f5e99885ba83512c19ee0d6e537f65cf
Kureev Alexey <kureev-mail@ya.ru>
2015-02-24 23:38:25 +0100

    Ignore node modules and examples initial commit

---
+++ examples/flux-todomvc/js/actions/TodoActions.js
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2014, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * TodoActions
+ */
+
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var TodoConstants = require('../constants/TodoConstants');
+
+var TodoActions = {
+
+  /**
+   * @param  {string} text
+   */
+  create: function(text) {
+    AppDispatcher.dispatch(TodoConstants.TODO_CREATE, {
+      text: text
+    });
+  },
+
+  /**
+   * @param  {string} id The ID of the ToDo item
+   * @param  {string} text
+   */
+  updateText: function(id, text) {
+    AppDispatcher.dispatch(TodoConstants.TODO_UPDATE_TEXT, {
+      id: id,
+      text: text
+    });
+  },
+
+  /**
+   * Toggle whether a single ToDo is complete
+   * @param  {object} todo
+   */
+  toggleComplete: function(todo) {
+    var id = todo.id;
+    if (todo.complete) {
+      AppDispatcher.dispatch(TodoConstants.TODO_UNDO_COMPLETE, {
+        id: id
+      });
+    } else {
+      AppDispatcher.dispatch(TodoConstants.TODO_COMPLETE, {
+        id: id
+      });
+    }
+  },
+
+  /**
+   * Mark all ToDos as complete
+   */
+  toggleCompleteAll: function() {
+    AppDispatcher.dispatch(TodoConstants.TODO_TOGGLE_COMPLETE_ALL);
+  },
+
+  /**
+   * @param  {string} id
+   */
+  destroy: function(id) {
+    AppDispatcher.dispatch(TodoConstants.TODO_DESTROY, {
+      id: id
+    });
+  },
+
+  /**
+   * Delete all the completed ToDos
+   */
+  destroyCompleted: function() {
+    AppDispatcher.dispatch(TodoConstants.TODO_DESTROY_COMPLETED);
+  }
+
+};
+
+module.exports = TodoActions;
