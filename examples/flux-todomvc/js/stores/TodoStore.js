fdae42a746a3be1da73df56dac40dff05046123a
Alexey Kureev <a.kureev@xs2theworld.com>
2015-02-27 18:15:57 +0100

    Example copyrights and replace -> set

--- examples/flux-todomvc/js/stores/TodoStore.js
+++ examples/flux-todomvc/js/stores/TodoStore.js
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2014, Facebook, Inc.
+ * Copyright (c) 2015, Kureev Alexey
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
@@ -132,7 +133,7 @@ var TodoStore = new FFlux.Store({
    * @return {Void}                
    */
   filterTodos: function(filterFunction) {
-    this.replaceState({
+    this.setState({
       todos: this.state.get('todos').filter(filterFunction)
     });
 
